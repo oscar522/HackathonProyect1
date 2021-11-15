021c0cc6d64c461f24a343a01f9b9939e38ec9b8
Oscar Ivan Ballesteros Bohorquez <ext_oballesterosb@falabella.com.co>
2021-11-15 14:35:07 -0500

    Model and entity framework

---
+++ Services/TestConnection.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static WebApplication1.Data.Connection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using WebApplication1.Data;
+
+namespace WebApplication1.Services
+{
+    public class TestConnection
+    {
+
+        
+
+        public TestConnection()
+        {
+            
+        }
+
+        public string GetOrders()
+        {
+            //Connection _context = new Connection() ;
+            //try
+            //{
+            //    var xxxx = _context.Orders.ToList();
+            //}
+            //catch (Exception e)
+            //{
+            //    var eerror = e;
+
+
+            //}
+
+
+
+
+            return "";
+            
+            }
+
+    }
+}
