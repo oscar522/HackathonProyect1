deae3183126e1341961f687499b3e6b09eeb6afa
Fabian Becerra <ye_prog@hotmail.com>
2021-11-15 19:24:35 -0500

    Creación de clase de GroupOrders

--- Services/DefineRoute.cs
+++ Services/DefineRoute.cs
@@ -19,30 +19,62 @@ namespace WebApplication1.Services
             maxY = orders.Max(x => x.longitude);
             orderQty = orders.Count();
 
-            //Define la cantidad de ordenes que puede enviar
-            orderMax = DefineRoute.Verify(orderQty);
+            //Define la cantidad de ordenes que se puede enviar, la diferencia hay que descartar
+            GroupOrders groups = DefineRoute.Verify(orderQty);
 
+        /*
+            while (groups.Total() < orders.Count(x=>x.delivery_date.HasValue ))
+            {
+                orders.Where(x => (x.delivery_end_date > x.delivery_date)).OrderBy(x => x.longitude).FirstOrDefault().delivery_date = null;
+            }
+        */
             return orders;
         }
 
-        public static int Verify(int orderQty)
+        /// <summary>
+        /// Verifica la cantidad de ordenes que puede enviar optimizando el uso de los camiones
+        /// </summary>
+        /// <param name="orderQty">Cantidad de ordenes iniciales</param>
+        /// <returns> Retorna una clase en donde informa cuántas rutas hay que crear de 98, de 99 y de 100</returns>
+        public static GroupOrders Verify(int orderQty)
         {
-            if (orderQty < 98) return 0;
-            if ((orderQty % 100 == 0)||(orderQty % 99 == 0)|| (orderQty % 98 == 0)) return orderQty;
-            
-            int o1 = (orderQty / 100)*100 + Verify(orderQty % 100);
-            if (o1 == orderQty) return orderQty;
+            GroupOrders groups = new GroupOrders();
 
-            int o2 = (orderQty / 99)*99 + Verify(orderQty % 99);
-            if (o2 == orderQty) return orderQty; 
-            
-            int o3 = (orderQty / 98)*98 + Verify(orderQty % 98);
-            if (o3 == orderQty) return orderQty;
+            if (orderQty < 98) return groups;
+            if (orderQty % 100 == 0)
+            {
+                groups.group100 = (orderQty / 100);
+                return groups;
+            }
+            if (orderQty % 99 == 0)
+            {
+                groups.group99 = (orderQty / 99);
+                return groups;
+            }
+            if (orderQty % 98 == 0)
+            {
+                groups.group98 = (orderQty / 98);
+                return groups;
+            }
 
-            if (o1 < o2) o1 = o2;
-            if (o1 < o3) o1 = o3;
+
+            GroupOrders o1 = Verify(orderQty % 100);
+            o1.group100 = orderQty / 100;
+            if (o1.Total() == orderQty) return o1;
+
+            GroupOrders o2 = Verify(orderQty % 99);
+            o2.group99 = orderQty / 99;
+            if (o2.Total() == orderQty) return o2;
+
+            GroupOrders o3 = Verify(orderQty % 98);
+            o3.group98 = orderQty / 98;
+            if (o3.Total() == orderQty) return o3;
+
+            if (o1.Total() < o2.Total()) o1 = o2;
+            if (o1.Total() < o3.Total()) o1 = o3;
             return o1;
 
         }
+
     }
 }
