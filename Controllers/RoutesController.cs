786d216771b711f3d8300ad395bcc4abd67477ee
Oscar Ivan Ballesteros Bohorquez <ext_oballesterosb@falabella.com.co>
2021-11-15 19:38:27 -0500

    integracion 1

---
+++ Controllers/RoutesController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using WebApi.Models;
+using WebApi.Services;
+using WebApplication1.Data;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RoutesController : ControllerBase
+    {
+        private IUserService _userService;
+        private readonly Connection _context;
+
+        public RoutesController(IUserService userService, Connection context)
+        {
+            _userService = userService;
+            _context = context;
+
+        }
+
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate(AuthenticateRequest model)
+        {
+            var response = _userService.Authenticate(model);
+
+            if (response == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            return Ok(response);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult CreateRoutes(string DateRoutes )
+        {
+            try {
+                    RoutesService RoutesService_ = new RoutesService();
+                    var StatusResult = RoutesService_.RoutesCreate(_context, Convert.ToDateTime( DateRoutes));
+                return Ok(StatusResult);
+
+            } catch(Exception e ) {
+                return UnprocessableEntity(e); 
+            }
+           
+        }
+    }
+}
---
+++ Models/Resultado.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Resultado
+    {
+        public int id { get; set; }
+      
+	}
+}
